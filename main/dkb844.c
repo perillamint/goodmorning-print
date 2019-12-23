e0fa24488d8f5b9240c8bef321bc808a00cd2028
perillamint <perillamint@quendi.moe>
2019-12-23 16:00:14 +0900

    Little bit of code cleanup

--- main/dkb844.c
+++ main/dkb844.c
@@ -62,57 +62,56 @@ void dkb_init()
     }
 }
 
-int dkb_render(uint16_t code, uint8_t buf[32])
-{
-        int choIdx = 0;
-        int jungIdx = 0;
-        int jongIdx = 0;
+int dkb_render(uint16_t code, uint8_t buf[32]) {
+    int choIdx = 0;
+    int jungIdx = 0;
+    int jongIdx = 0;
 
-        memset(buf, 0, 32);
+    memset(buf, 0, 32);
 
-        if (code >= 0x1100 && code <= 0x1112) {
-            choIdx = code - 0x1100 + 1;
-        } else if (code >= 0x1161 && code <= 0x1175) {
-            jungIdx = code - 0x1161 + 1;
-        } else if (code >= 0x11A8 && code <= 0x11C2) {
-            jongIdx = code - 0x11A8 + 1;
-        } else if (code >= 0x3131 && code <= 0x314E) {
-            choIdx = compatChoIdxLookup[code - 0x3131];
-        } else if (code >= 0x314F && code <= 0x3163) {
-            jungIdx = code - 0x314F + 1;
-        } else if (code >= 0xAC00 && code <= 0xD7A3) {
-            uint16_t nchr = code - 0xAC00;
-            choIdx = nchr / (0x0015 * 0x001C) + 1;
-            jungIdx = (nchr / 0x001C) % 0x0015 + 1;
-            jongIdx = nchr % 0x001C;
-        } else {
-            return -1;
-        }
+    if (code >= 0x1100 && code <= 0x1112) {
+        choIdx = code - 0x1100 + 1;
+    } else if (code >= 0x1161 && code <= 0x1175) {
+        jungIdx = code - 0x1161 + 1;
+    } else if (code >= 0x11A8 && code <= 0x11C2) {
+        jongIdx = code - 0x11A8 + 1;
+    } else if (code >= 0x3131 && code <= 0x314E) {
+        choIdx = compatChoIdxLookup[code - 0x3131];
+    } else if (code >= 0x314F && code <= 0x3163) {
+        jungIdx = code - 0x314F + 1;
+    } else if (code >= 0xAC00 && code <= 0xD7A3) {
+        uint16_t nchr = code - 0xAC00;
+        choIdx = nchr / (0x0015 * 0x001C) + 1;
+        jungIdx = (nchr / 0x001C) % 0x0015 + 1;
+        jongIdx = nchr % 0x001C;
+    } else {
+        return -1;
+    }
 
-        uint8_t (*choSet)[CHARSZ] = rom_choSet[0];
-        uint8_t (*jungSet)[CHARSZ] = rom_jungSet[0];
-        uint8_t (*jongSet)[CHARSZ] = rom_jongSet[0];
+    uint8_t (*choSet)[CHARSZ] = rom_choSet[0];
+    uint8_t (*jungSet)[CHARSZ] = rom_jungSet[0];
+    uint8_t (*jongSet)[CHARSZ] = rom_jongSet[0];
 
-        if (choIdx != 0 && jungIdx == 0 && jongIdx == 0) {
-            choSet = rom_choSet[1];
-        } else if (choIdx == 0 && jungIdx != 0 && jongIdx == 0) {
-            jungSet = rom_jungSet[0];
-        } else if (choIdx == 0 && jungIdx == 0 && jongIdx == 0) {
-            jongSet = rom_jongSet[0];
-        } else if (choIdx != 0 && jungIdx != 0 && jongIdx == 0) {
-            choSet = rom_choSet[choLookup1[jungIdx]];
-            jungSet = rom_jungSet[jungLookup1[choIdx]];
-        } else if (choIdx != 0 && jungIdx != 0 && jongIdx != 0) {
-            choSet = rom_choSet[choLookup2[jungIdx]];
-            jungSet = rom_jungSet[jungLookup2[choIdx]];
-            jongSet = rom_jongSet[jongLookup[jungIdx]];
-        }
+    if (choIdx != 0 && jungIdx == 0 && jongIdx == 0) {
+        choSet = rom_choSet[1];
+    } else if (choIdx == 0 && jungIdx != 0 && jongIdx == 0) {
+        jungSet = rom_jungSet[0];
+    } else if (choIdx == 0 && jungIdx == 0 && jongIdx == 0) {
+        jongSet = rom_jongSet[0];
+    } else if (choIdx != 0 && jungIdx != 0 && jongIdx == 0) {
+        choSet = rom_choSet[choLookup1[jungIdx]];
+        jungSet = rom_jungSet[jungLookup1[choIdx]];
+    } else if (choIdx != 0 && jungIdx != 0 && jongIdx != 0) {
+        choSet = rom_choSet[choLookup2[jungIdx]];
+        jungSet = rom_jungSet[jungLookup2[choIdx]];
+        jongSet = rom_jongSet[jongLookup[jungIdx]];
+    }
 
-        for (int i = 0; i < CHARSZ; i++) {
-            buf[i] |= choSet[choIdx][i];
-            buf[i] |= jungSet[jungIdx][i];
-            buf[i] |= jongSet[jongIdx][i];
-        }
+    for (int i = 0; i < CHARSZ; i++) {
+        buf[i] |= choSet[choIdx][i];
+        buf[i] |= jungSet[jungIdx][i];
+        buf[i] |= jongSet[jongIdx][i];
+    }
 
-        return 0;
+    return 0;
 }
